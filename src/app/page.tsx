ee4dba53c72e46bb03a47475c2fa9341b2345e57
ajaywaghray <ajay.waghray@gmail.com>
2024-01-17 21:40:38 -0600

    Hope this impl of Select works!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,16 @@ import {
 export default function Home() {
   return (
     <main>
-      Home
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </main>
   )
 }
