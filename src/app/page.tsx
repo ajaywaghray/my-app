5dc5871549935776217082578b7f21a839b11e07
ajaywaghray <ajay.waghray@gmail.com>
2024-01-19 17:17:14 -0600

    Add login card to Insights tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 import {
   Select,
   SelectContent,
@@ -27,6 +29,19 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
+import { Input } from "@/components/ui/input"
+
+import { Label } from "@/components/ui/label"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function Home() {
   return (
     <main>
@@ -48,7 +63,42 @@ export default function Home() {
               <TabsTrigger value="data">Data</TabsTrigger>
               <TabsTrigger value="surveys">Surveys</TabsTrigger>
             </TabsList>
-            <TabsContent value="insights" style={{ textAlign: 'center' }}>Insights go here.</TabsContent>
+            <TabsContent value="insights">
+            <Card className="w-[350px]">
+              <CardHeader>
+                <CardTitle>Create project</CardTitle>
+                <CardDescription>Deploy your new project in one-click.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="name">Name</Label>
+                      <Input id="name" placeholder="Name of your project" />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="framework">Framework</Label>
+                      <Select>
+                        <SelectTrigger id="framework">
+                          <SelectValue placeholder="Select" />
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                          <SelectItem value="next">Next.js</SelectItem>
+                          <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                          <SelectItem value="astro">Astro</SelectItem>
+                          <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Deploy</Button>
+              </CardFooter>
+            </Card>
+            </TabsContent>
             <TabsContent value="personas" style={{ textAlign: 'center' }}>Persona creation goes here.</TabsContent>
             <TabsContent value="chat" style={{ textAlign: 'center' }}>Chats go here.</TabsContent>
             <TabsContent value="data" style={{ textAlign: 'center' }}>Data goes here.</TabsContent>
