e9105f648648bcf3771a236669ed622ef085bb08
ajaywaghray <ajay.waghray@gmail.com>
2024-01-19 13:02:22 -0600

    What happens here?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,11 @@ export default function Home() {
     <main>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top', padding: '16px' }}>
         <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}>
-          <div style={{ display: 'flex', alignItems: 'center', fontWeight: '700', fontSize: '24px' }}>
-            <img src="/Quikest Logo.svg" alt="Image description" style={{ height: '20px', width: '20px', marginRight: '4px' }} />
-            Quikest
+          <div style={{ display: 'flex', alignItems: 'center'}}>
+            <img src="/Quikest Logo.svg" alt="Image description" style={{ height: '32px', width: '32px', marginRight: '4px' }} />
+            <div style={{ fontWeight: '700', fontSize: '24px' }}>
+              Quikest
+            </div>
           </div>
         </div>
         <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
