739cfa96495df9e620e502f6ff424b54cef3dfb9
ajaywaghray <ajay.waghray@gmail.com>
2024-01-20 07:19:04 -0600

    Trying to get modal centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,42 +64,42 @@ export default function Home() {
               <TabsTrigger value="surveys">Surveys</TabsTrigger>
             </TabsList>
             <TabsContent value="insights">
-            <div style={{ flex: 1, display: 'flex', width: '100vh', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
-              <Card className="w-[350px]">
-                <CardHeader>
-                  <CardTitle>Create project</CardTitle>
-                  <CardDescription>Deploy your new project in one-click.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <form>
-                    <div className="grid w-full items-center gap-4">
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">Name</Label>
-                        <Input id="name" placeholder="Name of your project" />
+              <div style={{ flex: 1, display: 'flex', width: '33vh', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
+                <Card className="w-[350px]">
+                  <CardHeader>
+                    <CardTitle>Create project</CardTitle>
+                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <form>
+                      <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                          <Label htmlFor="name">Name</Label>
+                          <Input id="name" placeholder="Name of your project" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                          <Label htmlFor="framework">Framework</Label>
+                          <Select>
+                            <SelectTrigger id="framework">
+                              <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent position="popper">
+                              <SelectItem value="next">Next.js</SelectItem>
+                              <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                              <SelectItem value="astro">Astro</SelectItem>
+                              <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </div>
                       </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="framework">Framework</Label>
-                        <Select>
-                          <SelectTrigger id="framework">
-                            <SelectValue placeholder="Select" />
-                          </SelectTrigger>
-                          <SelectContent position="popper">
-                            <SelectItem value="next">Next.js</SelectItem>
-                            <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                            <SelectItem value="astro">Astro</SelectItem>
-                            <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </div>
-                    </div>
-                  </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                  <Button variant="outline">Cancel</Button>
-                  <Button>Deploy</Button>
-                </CardFooter>
-              </Card>
-            </div>
+                    </form>
+                  </CardContent>
+                  <CardFooter className="flex justify-between">
+                    <Button variant="outline">Cancel</Button>
+                    <Button>Deploy</Button>
+                  </CardFooter>
+                </Card>
+              </div>
             </TabsContent>
             <TabsContent value="personas" style={{ textAlign: 'center' }}>Persona creation goes here.</TabsContent>
             <TabsContent value="chat" style={{ textAlign: 'center' }}>Chats go here.</TabsContent>
