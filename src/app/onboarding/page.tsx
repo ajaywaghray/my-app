9c6d6e0fef15de05ddfe740d53b93060266380c8
ajaywaghray <ajay.waghray@gmail.com>
2024-01-23 21:33:02 -0600

    Adding header

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,8 +3,8 @@ import * as React from "react"
 export default function Onboarding () {
   return (
     <main>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        Onboarding
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: '600', fontSize: '30px' }}>
+        Welcome to Quikest!
       </div>
     </main>
   )
