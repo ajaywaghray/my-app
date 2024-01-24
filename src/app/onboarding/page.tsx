6988590c2ac9e021f3853fd7fcc4b507eaea08c9
ajaywaghray <ajay.waghray@gmail.com>
2024-01-23 21:35:34 -0600

    Add header to onboarding

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,6 +3,14 @@ import * as React from "react"
 export default function Onboarding () {
   return (
     <main>
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}>
+        <div style={{ display: 'flex', alignItems: 'top', height: '50px'}}>
+          <img src="/Quikest Logo.svg" alt="Image description" style={{ height: '32px', width: '32px', marginRight: '4px', marginTop: '4px' }} />
+          <div style={{ fontWeight: '700', fontSize: '24px' }}>
+            Quikest
+          </div>
+        </div>
+      </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: '600', fontSize: '30px' }}>
         Welcome to Quikest!
       </div>
