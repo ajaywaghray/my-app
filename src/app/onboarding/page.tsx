c69390d2f6bcd178a0fd426594faed1627d8de67
ajaywaghray <ajay.waghray@gmail.com>
2024-01-23 21:38:04 -0600

    Added header div with padding

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,11 +3,13 @@ import * as React from "react"
 export default function Onboarding () {
   return (
     <main>
-      <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}>
-        <div style={{ display: 'flex', alignItems: 'top', height: '50px'}}>
-          <img src="/Quikest Logo.svg" alt="Image description" style={{ height: '32px', width: '32px', marginRight: '4px', marginTop: '4px' }} />
-          <div style={{ fontWeight: '700', fontSize: '24px' }}>
-            Quikest
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top', padding: '16px' }}>
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}>
+          <div style={{ display: 'flex', alignItems: 'top', height: '50px'}}>
+            <img src="/Quikest Logo.svg" alt="Image description" style={{ height: '32px', width: '32px', marginRight: '4px', marginTop: '4px' }} />
+            <div style={{ fontWeight: '700', fontSize: '24px' }}>
+              Quikest
+            </div>
           </div>
         </div>
       </div>
