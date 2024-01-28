58bd56af4fd57299403ba22493231f7ccaa1360a
ajaywaghray <ajay.waghray@gmail.com>
2024-01-27 21:56:35 -0600

    Dark mode selector?!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+"use client"
+
 import * as React from "react"
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+
+import { useTheme } from "next-themes"
 
 import { Checkbox } from "@/components/ui/checkbox"
 
@@ -47,6 +53,8 @@ import {
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { setTheme } = useTheme()
+
   return (
     <main>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top', padding: '16px' }}>
@@ -99,6 +107,28 @@ export default function Home() {
           </Tabs>
         </div>
         <div style={{ flex: 1, display: 'flex', justifyContent: 'right', height: '50px' }}>
+          <div style={{ flex: 1, display: 'flex', justifyContent: 'right', height: '50px' }}>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
           <div style={{ marginRight: '8px' }}><UserButton afterSignOutUrl="/"/></div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
