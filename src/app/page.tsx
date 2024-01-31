6523087d7aff9de4f31c7e3b18abdf1cfc517df8
ajaywaghray <ajay.waghray@gmail.com>
2024-01-30 21:38:47 -0600

    Clean up project, remove dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,55 +4,7 @@ import * as React from "react"
 
 import HeaderComponent from "../components/common/header";
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-
 import { useTheme } from "next-themes"
-
-import { Checkbox } from "@/components/ui/checkbox"
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-import { Button } from "@/components/ui/button"
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
-import { Input } from "@/components/ui/input"
-
-import { Label } from "@/components/ui/label"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const { setTheme } = useTheme()
