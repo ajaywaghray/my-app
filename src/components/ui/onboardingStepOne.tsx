f4851bbe0646e679e9a02c440a18097e8103b4ce
ajaywaghray <ajay.waghray@gmail.com>
2024-01-31 23:27:57 -0600

    Form button setup

--- src/components/ui/onboardingStepOne.tsx
+++ src/components/ui/onboardingStepOne.tsx
@@ -6,6 +6,13 @@ import { useState } from 'react';
 
 import { Checkbox } from "@/components/ui/checkbox"
 
+import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { toast } from "@/components/ui/use-toast"
+
 import {
   Select,
   SelectContent,
@@ -48,12 +55,43 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+
 import { UserButton } from "@clerk/nextjs";
 import OnboardingHeaderComponent from "../common/onboardingHeader";
+
+const FormSchema = z.object({
+  mobile: z.boolean().default(false).optional(),
+})
 
 const OnboardingStepOne = ({ onNext }: { onNext: () => void; }) => {
   // Add state management and form handling here
   const [state, setState] = React.useState();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      mobile: true,
+    },
+  })
+ 
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
 
   return (
     <main>
@@ -75,13 +113,31 @@ const OnboardingStepOne = ({ onNext }: { onNext: () => void; }) => {
             <div style={{ display: 'flex', height: '24px', fontSize: '16px', fontWeight: '700' }}>
               Whats your role?
             </div>
-            <Checkbox id="founder" />
-              <label
-                htmlFor="founder"
-                className="text-sm font-medium border-primary leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Founder
-              </label>
+            
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                <FormField
+                  control={form.control}
+                  name="mobile"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>
+                          Founder
+                        </FormLabel>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+
           </CardContent>
           <CardFooter className="flex justify-between">
           </CardFooter>
