db6433e980b3ba6b7dc4b28da197a492ea2a4d34
ajaywaghray <ajay.waghray@gmail.com>
2024-02-03 15:04:58 -0600

    Other button

--- src/components/ui/onboardingStepOne.tsx
+++ src/components/ui/onboardingStepOne.tsx
@@ -261,6 +261,16 @@ const OnboardingStepOne = ({ onNext }: { onNext: () => void; }) => {
                   Content Strategy
                 </label>
               </div>
+
+              <div className="flex items-center space-x-2 border-2 border-zinc-300 dark:border-zinc-600 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-lg" style={{ padding: '8px', marginTop: '8px' }}>      
+                <Checkbox id="other" className="border-2 border-zinc-400 dark:border-zinc-500"/>
+                <label
+                  htmlFor="other"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Other
+                </label>
+              </div>
               
             </div>
             <div>
