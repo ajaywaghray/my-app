d129d1ae1da6f9ca26c6af70de4beb0f8d58ae7b
ajaywaghray <ajay.waghray@gmail.com>
2024-02-12 21:12:32 -0600

    Maybe stuff will get stored in the database?!

--- src/app/api/onboarding-step-one-store/route.ts
+++ src/app/api/onboarding-step-one-store/route.ts
@@ -6,14 +6,14 @@ import type { NextRequest } from 'next/server';
 
 import { NextApiResponse, NextApiRequest } from 'next';
 
-async function storeSelections(workspace_id: any, user_id: any, selections: any) {
+async function storeSelections(workspace_id: any, user_id: any, user_email_address: any, selections: any) {
   
   console.log("storeSelections function called");
 
   // Store selections from onboarding in database
   try {
     console.log("Starting SQL query to store selections in database");
-    await sql`INSERT INTO quikest (workspace_id, user_id, onboarding_step_one) VALUES (1, 1, ${selections});`;
+    await sql`INSERT INTO quikest (workspace_id, user_id, user_email_address, onboarding_step_one) VALUES (1, 1, ${selections});`;
   } catch (error) {
     console.log("Selections not stored in database due to error: " + error);
     return NextResponse.json({ error }, { status: 500 });
@@ -25,11 +25,6 @@ async function storeSelections(workspace_id: any, user_id: any, selections: any)
 
 export async function POST (request: Request) {
   console.log("POST function called");
-
-  //Delete existing quikest table
-  const deleteTable = await sql`
-    DROP TABLE IF EXISTS quikest;
-  `;
   
   //Create a table if none exists
   const createTable = await sql`
@@ -43,13 +38,11 @@ export async function POST (request: Request) {
 
   console.log("Table created if there wasn't one already");
 
-  
-  
   // Receive array of what do you do inputs
   const { selections } = await request.json();
 
   // Store the selections in the database against the new user ID and create a new workspace ID to store in the database
-  await storeSelections(selections, selections, selections);
+  await storeSelections(selections, selections, selections, selections);
 
   // Return a response that the selections have been stored
   return new Response('Selections stored', { status: 200 });
