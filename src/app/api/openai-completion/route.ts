f8864d2723857eac0f271058e73db4550d72d1e5
ajaywaghray <ajay.waghray@gmail.com>
2024-02-20 20:45:31 -0600

    Only receiving message

--- src/app/api/openai-completion/route.ts
+++ src/app/api/openai-completion/route.ts
@@ -2,37 +2,34 @@ import OpenAI from 'openai';
 
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-import { NextResponse } from 'next/server';
-
-import type { NextRequest } from 'next/server';
-
-import { sql } from '@vercel/postgres';
-
-import { NextApiResponse, NextApiRequest } from 'next';
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
- 
-// Set the runtime to edge for best performance
-export const runtime = 'edge';
 
 export async function POST (request: Request) {
 
     console.log("openaiCompletion function called");
 
     // Receive the user ID and the company name
-    const { messages } = await request.json();
+    const { message } = await request.json();
 
-    console.log("Sending the following messages to OpenAI: " + messages.role + " " + messages.content);
+    console.log("Sending the following messages to OpenAI: " + message);
 
     // Request the OpenAI API for the response based on the prompt
     const response = await openai.chat.completions.create({
         model: 'gpt-4-0125-preview',
         max_tokens: 500,
         stream: true,
-        messages,
+        messages: [
+            {
+              role: 'user',
+              content: message,
+            },
+          ],
     });
 
     console.log("OpenAI response: " + response );
