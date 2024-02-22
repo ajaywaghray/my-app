af2f3937df1fa9bf9598e23082c749b7281c36f1
ajaywaghray <ajay.waghray@gmail.com>
2024-02-21 21:24:57 -0600

    Let's see what the req is

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -14,6 +14,8 @@ export async function POST (req: Request) {
 
     console.log("openaiCompletion function called");
 
+    console.log("req: ", req );
+
     // Receive the user ID and the company name
     const { message } = await req.json();
 
