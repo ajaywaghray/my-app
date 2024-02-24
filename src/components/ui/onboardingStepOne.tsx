4dedcc6ecdd1c8b5044f55e7f04a246b23aa0a5b
ajaywaghray <ajay.waghray@gmail.com>
2024-02-24 07:22:08 -0600

    top align middle?

--- src/components/ui/onboardingStepOne.tsx
+++ src/components/ui/onboardingStepOne.tsx
@@ -315,8 +315,8 @@ const OnboardingStepOne = ({ onNext }: { onNext: () => void; }) => {
                 </label>
               </div>
 
-              <div className="flex items-center space-x-2 border-2 border-zinc-300 dark:border-zinc-600 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-lg" style={{ padding: '8px' }}>      
-                <Checkbox onClick={handleCheckboxChangeClevel} id="clevel" className="border-2 border-zinc-400 dark:border-zinc-500 align-top"/>
+              <div className="flex items-center space-x-2 border-2 border-zinc-300 dark:border-zinc-600 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-lg align-middle" style={{ padding: '8px' }}>      
+                <Checkbox onClick={handleCheckboxChangeClevel} id="clevel" className="border-2 border-zinc-400 dark:border-zinc-500"/>
                 <label
                   htmlFor="clevel"
                   className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 align-middle"
