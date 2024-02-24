fda7d5ec8786b15bcc1ea678d94250572b05e5e0
ajaywaghray <ajay.waghray@gmail.com>
2024-02-24 07:35:51 -0600

    Align all the things middle

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -19,9 +19,9 @@ const Checkbox = React.forwardRef<
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
+      className={cn("flex items-center justify-center text-current align-middle")}
     >
-      <CheckIcon className="h-4 w-4" />
+      <CheckIcon className="h-4 w-4 align-middle" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
